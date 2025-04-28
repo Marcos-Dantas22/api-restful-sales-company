53340ce470ac3554142bd6157b461c17683a911e
Marcos-Dantas22 <marcosdantaslkdin@gmail.com>
2025-04-28 09:40:10 -0300

    added code to create admin user in script start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@
 echo "▶ Rodando as migrations..."
 alembic upgrade head
 
+echo "👤 Criando usuário admin padrão..."
+python create_admin_user.py
+
 echo "🚀 Iniciando servidor FastAPI..."
 exec uvicorn api_restful.main:app --host 0.0.0.0 --port 8000
